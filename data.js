382c18e8e8463145d6c9c4f790628ff29fe1f47a
Michael van Meerwijk <michaelvanm@gmail.com>
2017-12-12 19:33:37 +0100

    prototypes created for sessionTypes

--- data.js
+++ data.js
@@ -1,16 +1,6 @@
 /*jshint devel: true, esversion: 6, browser: true*/
 /* globals controller */
 
-/*
-but also long-term information:
-    totalTimePlayed
-    totalSessions
-    amountOfPauses
-    timeWorked
-    timePaused
-    etc etc
-*/
-
 var data = (function() {
     //session variable contains all information about the session types
     var sessionTypes = {};
@@ -19,12 +9,72 @@ var data = (function() {
     function Session(name, fullName, initialDur, maxDur, minDur) {
         this.name = name;
         this.fullName = fullName;
-        this.dur = {};
-        this.dur.current = convertToMS(initialDur);
-        this.dur.initial = this.dur.current;
-        this.dur.max = convertToMS(maxDur);
-        this.dur.min = convertToMS(minDur);
+        this.dur = {
+            current: initialDur,
+            initial: initialDur,
+            max: maxDur,
+            min: minDur,
+            //if the increase and decrease buttons should be/are disabled
+            buttonsDisabled: {
+                increase: false,
+                decrease: false
+            }
+        };
     }
+    
+    //prototype method to reset the duration of a method
+    Session.prototype.resetDur = function() {
+        this.dur.current = this.dur.initial;
+        this.changeButtonState();
+    };
+    
+    //prototype methods to change disabling of buttons (if there is a change)
+    Session.prototype.changeButtonState = function() {
+        let iBut = this.dur.buttonsDisabled.increase;
+        let dBut = this.dur.buttonsDisabled.decrease;
+        
+        let maxReached = this.dur.current >= this.dur.max ? true : false;
+        let minReached = this.dur.current <= this.dur.min ? true : false;
+        
+        //TODO: send signal to controller/view
+        if (maxReached === true && iBut === false) {
+            //controller must disable button
+        } else if (maxReached === false && iBut === true) {
+            //controller must enable button
+        }
+        
+        if (minReached === true && dBut === false) {
+            //controller must disable button
+        } else if (minReached === false && iBut === true) {
+            //controller must enable button
+        }
+        
+        this.dur.buttonsDisabled.increase = maxReached;
+        this.dur.buttonsDisabled.decrease = minReached;
+    };
+    
+    //prototype methods to change current duration
+    Session.prototype.increaseDur = function(minutes) {
+        if (this.dur.current + minutes < this.dur.max) {
+            this.dur.current += minutes;
+        } else if (this.dur.current + minutes === this.dur.max) {
+            this.dur.current += minutes;
+        } else {
+            return false;
+        }
+        this.changeButtonState();
+    };
+    
+    Session.prototype.decreaseDur = function(minutes) {
+        if (this.dur.current - minutes > this.dur.min) {
+            this.dur.current -= minutes;
+        } else if (this.dur.current - minutes === this.dur.min) {
+            this.dur.current -= minutes;
+        } else {
+            return false;
+        }
+        this.changeButtonState();
+    };
     
     //timerData variable holds all the other data needed, such as cycleArray, current session number, and time left
     var timerData = {
@@ -35,7 +85,15 @@ var data = (function() {
         playing: false,
         cycle: {
             length: 3,
-            sessions: []
+            sessions: [],
+            get totalSessions() {
+                if (this.length*2 === this.sessions.length) {
+                    return length * 2;
+                } else {
+                    console.log("Error! Length of cycle, and the amount of sessions in the sessions array do not match up!");
+                    return undefined;
+                }                
+            }
         }        
     };
     
@@ -44,6 +102,16 @@ var data = (function() {
         this.name = type;
         this.hasStarted = false;
         this.hasFinished = false;
+    }
+    
+    //init function, only to be used when all modules are loaded
+    function init() {
+        sessionTypes.work = new Session("work", "Work", 25, 60, 15);
+        sessionTypes.short = new Session("short", "Short Break", 5, 15, 2);
+        sessionTypes.long = new Session("long", "Long Break", 20, 60, 5);
+        
+        //TODO: set timeLeft variable
+        //TODO: generate cycle
     }
     
     //time conversion functions
@@ -63,60 +131,6 @@ var data = (function() {
 
 /*
 var dataLegacy = (function() {
-    Session.prototype.maxReached = function(yesOrNo) {
-        if (yesOrNo === this.dur.maxLimitReached) {
-            return;
-        } else {
-            this.dur.maxLimitReached = yesOrNo;
-            controller.checkDurationDisabled(this.name, this.dur.maxLimitReached, "max");
-        }
-    };
-    
-    Session.prototype.minReached = function(yesOrNo) {
-        if (yesOrNo === this.dur.minLimitReached) {
-            return;
-        } else {
-            this.dur.minLimitReached = yesOrNo;
-            controller.checkDurationDisabled(this.name, this.dur.minLimitReached, "min");
-        }
-    };
-    
-    //adds method to prototype to reset duration to initial duration
-    Session.prototype.resetDur = function() {
-        this.dur.current = this.dur.initial;
-        this.maxReached(false);
-        this.minReached(false);
-        console.log("Duration of " + this.fullName + " has been reset to " + this.dur.current);
-    };
-    
-    Session.prototype.increaseDur = function(amount) {
-        if (amount < 60000) {
-            amount = 60000;
-        }
-        
-        if (this.dur.current + amount > this.dur.max) {
-            console.log("Error! Duration of " + this.fullName + " could not increase beyond maximum.");
-        } else {
-            this.dur.current += amount;
-            console.log("Duration of " + this.fullName + " has been increased by one minute to " + this.dur.current + " ms");
-        }
-    };
-    
-    Session.prototype.decreaseDur = function(amount) {
-        if (amount > -60000) {
-            amount = -60000;
-        }
-        
-        amount = amount * -1;
-        
-        if (this.dur.current - amount < this.dur.min) {
-            console.log("Error! Duration of " + this.fullName + " could not decrease beyond minimum.");
-        } else {
-            this.dur.current -= amount;
-            console.log("Duration of " + this.fullName + " has been decreased by one minute to " + this.dur.current + " ms");
-        }
-    };
-    
     //creates the three session types and assigns them to the above "sessions" variable
     sessions.work = new Session("work", "Work", 25, 60, 15);
     sessions.short = new Session("short", "Short Break", 5, 15, 2);
@@ -413,6 +427,17 @@ var dataLegacy = (function() {
     
 })();
 
+*/
+
+
+/*
+but also long-term information:
+    totalTimePlayed
+    totalSessions
+    amountOfPauses
+    timeWorked
+    timePaused
+    etc etc
 */
 
 
